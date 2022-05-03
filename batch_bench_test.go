30d9b4d8d958bf252aefdde24bc2f4cf63bd9722
Jacek Olszak <jacekolszak@gmail.com>
2022-05-03 10:39:45 +0200

    Add benchmark

---
+++ batch_bench_test.go
@@ -0,0 +1,47 @@
+package batch_test
+
+import (
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/elgopher/batch"
+	"github.com/stretchr/testify/require"
+)
+
+func BenchmarkProcessor_Run(b *testing.B) {
+	resources := []int{
+		1, 8, 64, 512, 4096, 32768, 262144, 2097152,
+	}
+
+	for _, resourceCount := range resources {
+		b.Run(strconv.Itoa(resourceCount), func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			processor := batch.StartProcessor(batch.Options[empty]{})
+			defer processor.Stop()
+
+			var allOperationsFinished sync.WaitGroup
+			allOperationsFinished.Add(b.N)
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				key := strconv.Itoa(i % resourceCount)
+				go func() {
+					// when
+					err := processor.Run(key, operation)
+					require.NoError(b, err)
+					allOperationsFinished.Done()
+				}()
+			}
+
+			b.StopTimer()
+
+			allOperationsFinished.Wait()
+		})
+	}
+}
+
+func operation(empty) {}
